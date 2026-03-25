b239fba49d1e027514250dca73740c1f377b58ae
Link4Electronics <link4electronics@gmail.com>
2026-03-25 08:52:02 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q qemu-system-x86 | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://gitlab.com/qemu-project/qemu/-/raw/master/ui/icons/qemu.svg
+export DESKTOP=DUMMY
+export MAINBIN=qemu-syste-x86_64
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/qemu-system-x86_64 /usr/bin/qemu-system-i386
 
 # Additional changes can be done in between here
 
