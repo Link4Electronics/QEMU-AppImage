c128794eee624a6c9ad05d7e9468045d825f5b5a
Link4Electronics <link4electronics@gmail.com>
2026-03-25 14:20:32 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,6 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
+if [ "$ARCH" = "aarch64" ]; then
+    wget https://umea.mirror.pkgbuild.com/extra/os/x86_64/edk2-ovmf-202508-1-any.pkg.tar.zst
+    wget https://umea.mirror.pkgbuild.com/extra/os/x86_64/seabios-1.17.0-2-any.pkg.tar.zst
+    pacman -U seabios-*.pkg.tar.zst edk2-ovmf-*.pkg.tar.zst
+fi
 pacman -Syu --noconfirm      \
     gtk3                     \
     libdecor                 \
@@ -22,11 +27,6 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
-if [ "$ARCH" = "aarch64" ]; then
-    wget https://umea.mirror.pkgbuild.com/extra/os/x86_64/edk2-ovmf-202508-1-any.pkg.tar.zst
-    wget https://umea.mirror.pkgbuild.com/extra/os/x86_64/seabios-1.17.0-2-any.pkg.tar.zst
-    pacman -U seabios-*.pkg.tar.zst edk2-ovmf-*.pkg.tar.zst
-fi
 
 # If the application needs to be manually built that has to be done down here
 #mkdir -p ./AppDir/bin
